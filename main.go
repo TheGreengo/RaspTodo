f61a1c5de89dc9599dd294986706a9dce8a46fab
TheGreengo <bradenoptimus@gmail.com>
2024-09-26 06:17:14 +0100

    we stylin

--- main.go
+++ main.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
+	"html/template"
 )
 
-func getRoot(w http.ResponseWriter, r * http.Request) {
-	fmt.Println("access")
-	io.WriteString(w, "this is working")
+// Plans are made to be broken
+// - figure out some interactivity (forms)
+// - get a database working
+// - figure out what we actually want to do
+
+type Test struct {
+	Thing string
+}
+
+func getMain(w http.ResponseWriter, r * http.Request) {
+	fmt.Println("Accessing main page")
+
+	t, _ := template.ParseFiles("index.html")
+	thing := Test{"Your mom"}
+	t.Execute(w, thing)
 }
 
 func main() {
-	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/", getMain)
+
 	fmt.Println("Listening at 8080")
+
+	styles := "/static/css"
+	strip := http.StripPrefix(styles, http.FileServer(http.Dir(styles[1:])))
+    http.Handle("/static/css/", strip)
 	http.ListenAndServe(":8080", nil)
 }
